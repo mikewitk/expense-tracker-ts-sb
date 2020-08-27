202148dc87eb809576d8cae8a9eea84f9fde54b6
Michael Witkowsky <mike.witk@gmail.com>
2020-08-26 21:12:56 -0400

    Files Clean up and SC + SB setup

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,20 @@
+const webpack = require('webpack')
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
-  ]
+  webpackFinal: async (config, { configType }) => {
+    config.plugins.push(
+      // Removing Speedy so the static storybook styling doesn't break
+      new webpack.DefinePlugin({
+        SC_DISABLE_SPEEDY: true,
+      })
+    )
+
+    return config
+  },
 }
